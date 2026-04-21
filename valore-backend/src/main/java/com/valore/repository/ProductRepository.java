a31414b77c0b738734dcbd56d8a359b4ad12905e
monssefbaakka <baakkamonssef@gmail.com>
2026-04-21 18:18:19 +0100

    chore: Update valore-backend/src/main/java/com/valore/repository/ProductRepository.java

--- valore-backend/src/main/java/com/valore/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.valore.repository;
-
-import com.valore.model.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
